699a48ce757106e67740b43a1e4756176cddbb3c
Bill Klinkow and Joshua Rinard <bill@>
2017-02-06 14:55:11 -0800

    crud functionality for listings

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,39 @@
+class ListingsController < ApplicationController
+  def new
+      @type = Type.find(params[:type_id])
+      @listing = @type.listings.new
+    end
+    def create
+      @type = Type.find(params[:type_id])
+      @listing = @type.listings.new(listing_params)
+      if @listing.save
+        redirect_to type_path(@listing.type)
+      else
+        render :new
+      end
+    end
+    def edit
+      @type = Type.find(params[:type_id])
+      @listing = Listing.find(params[:id])
+      render :edit
+    end
+    def update
+      @type = Type.find(params[:type_id])
+      @listing = Listing.find(params[:id])
+      if @listing.update(listing_params)
+        redirect_to type_path(@listing.type)
+      else
+        render :edit
+      end
+    end
+    def destroy
+      @type = Type.find(params[:type_id])
+      @listing = Listing.find(params[:id])
+      @listing.destroy
+      redirect_to types_path
+    end
+  private
+    def listing_params
+      params.require(:listing).permit(:name, :address, :phone, :web, :hours, :type_id)
+    end
+  end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,4 @@
 class Type < ActiveRecord::Base
-  # has_many :listings
+  has_many :listings
   validates :category, :presence => true
 end
